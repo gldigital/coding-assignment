6a27deb9d557d0b5f4aa29a81548bd41c9d55cc7
Greg <greg@Gregs-iMac.attlocal.net>
2018-09-10 21:54:30 -0500

    adding switch case

--- logic.js
+++ logic.js
@@ -1,29 +1,35 @@
 $(document).ready(function() {
-  // Change boxes to red
-  $("#redBtn").click(function() {
-    $("#redBox").css("background-color", "red");
-    $("#greenBox").css("background-color", "red");
-    $("#blueBox").css("background-color", "red");
-  });
-
-  // Change boxes to green
-  $("#greenBtn").click(function() {
-    $("#greenBox").css("background-color", "green");
-    $("#redBox").css("background-color", "green");
-    $("#blueBox").css("background-color", "green");
-  });
-
-  // Change boxes to blue
-  $("#blueBtn").click(function() {
-    $("#blueBox").css("background-color", "blue");
-    $("#redBox").css("background-color", "blue");
-    $("#greenBox").css("background-color", "blue");
-  });
-
-  // Reset box colors
-  $("#whiteBtn").click(function() {
-    $("#redBox").css("background-color", "red");
-    $("#greenBox").css("background-color", "green");
-    $("#blueBox").css("background-color", "blue");
+  
+  $("#redBtn, #greenBtn, #blueBtn, #whiteBtn").click(function() {
+    switch (this.id) {
+      case "redBtn":
+        {
+          $("#redBox").css("background-color", "red");
+          $("#greenBox").css("background-color", "red");
+          $("#blueBox").css("background-color", "red");
+        }
+        break;
+      case "greenBtn":
+        {
+          $("#greenBox").css("background-color", "green");
+          $("#redBox").css("background-color", "green");
+          $("#blueBox").css("background-color", "green");
+        }
+        break;
+      case "blueBtn":
+        {
+          $("#blueBox").css("background-color", "blue");
+          $("#redBox").css("background-color", "blue");
+          $("#greenBox").css("background-color", "blue");
+        }
+        break;
+      case "whiteBtn":
+        {
+          $("#redBox").css("background-color", "red");
+          $("#greenBox").css("background-color", "green");
+          $("#blueBox").css("background-color", "blue");
+        }
+        break;
+    }
   });
 });
